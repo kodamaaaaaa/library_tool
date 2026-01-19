811d495253132adff4da760213c5bed695b36d76
kodamaaaaaa <yzhangze14@gmail.com>
2026-01-19 13:12:21 +0900

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,16 @@
+'use strict';
+    function nextPage() {
+        const ids = ["21001", "23002"];
+        const passes = ["573837", "582758"];
+
+        const id = document.getElementById("idText").value;
+        const pwd = document.getElementById("passText").value;
+
+        if (id === ids[0] && pwd === passes[0]) {
+        location.href = 'main.html';
+        } else if (id === ids[1] && pwd === passes[1]) {
+        location.href = 'main1-2.html';
+        } else {
+        location.href = 'error.html';
+        }
+}
