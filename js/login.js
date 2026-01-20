41fdad04d7498b5bcce44145a475c094516c52dd
kodamaaaaaa <yzhangze14@gmail.com>
2026-01-20 09:40:28 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,18 +1,18 @@
 'use strict';
-    function nextPage() {
-        const ids = ["21001", "23002"];
-        const passes = ["573837", "582758"];
+function nextPage() {
 
-        const id = document.getElementById("idText").value;
-        const pwd = document.getElementById("passText").value;
+    let ids = ["21001", "23002"];
+    let passes = ["573837", "582758"];
 
-        if (id === ids[0] && pwd === passes[0]) {
-        location.href = 'main.html';
-        } else if (id === ids[1] && pwd === passes[1]) {
-        location.href = 'main1-2.html';
-        } else {
-        location.href = 'error.html';
-        }
+    const id = document.getElementById("idText").value;
+    const pwd = document.getElementById("passText").value;
 
+    if (id === ids[0] && pwd === passes[0]) {
+    location.href = 'main.html';
+    } else if (id === ids[1] && pwd === passes[1]) {
+    location.href = 'main1-2.html';
+    } else {
+    location.href = 'error.html';
+    }
 }
 
